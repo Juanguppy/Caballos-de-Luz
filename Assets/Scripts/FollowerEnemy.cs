d3d6ce6c22dc6052d13b2b82aaba479c4c265836
Juanguppy <almodovar.juan7@gmail.com>
2025-04-12 17:00:20 +0200

    new level completed

--- Assets/Scripts/FollowerEnemy.cs
+++ Assets/Scripts/FollowerEnemy.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class FollowerEnemy : BasciEnemy
 {
-    public float speed = 7.5f;
-    public float detectionRange = 10f; 
-    private Transform playerTransform;
+
+    private Vector3 initialPosition; 
+    private float returnThreshold = 0.5f;
+
+    protected override void Start()
+    {
+        base.Start();
+        animator.SetBool("idle", true);
+
+        initialPosition = transform.position;
+    }
 
     protected override void Move()
     {
@@ -14,15 +22,32 @@ public class FollowerEnemy : BasciEnemy
             float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
 
             // Si el jugador está dentro del rango de detección, perseguirlo
-            if (distanceToPlayer <= detectionRange)
-            {
+            if (distanceToPlayer <= detectionRange) {
                 Vector3 directionToPlayer = (playerTransform.position - transform.position).normalized;
                 transform.position += directionToPlayer * speed * Time.deltaTime;
+                animator.SetBool("idle", false);
 
                 // Rotar hacia el jugador
                 Quaternion lookRotation = Quaternion.LookRotation(directionToPlayer);
                 transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+            } else if (Vector3.Distance(transform.position, initialPosition) > returnThreshold) {
+                Vector3 directionToInitial = (initialPosition - transform.position).normalized;
+                transform.position += directionToInitial * speed * Time.deltaTime;
+                animator.SetBool("idle", false);
+
+                Quaternion lookRotation = Quaternion.LookRotation(directionToInitial);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+            } else {
+                animator.SetBool("idle", true);
             }
         }
+    }
+
+    // para dibujar el rango de detección en la escena
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
     }
 }
